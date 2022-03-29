f8acc40f96e89529ae5bc6ec84598ef2c8554e84
amit Kumar singh <amitkumar95041869@gmail.com>
2022-03-29 15:22:42 +0530

    Add project files.

---
+++ EntityCRUD/Models/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntityCRUD.Models
+{
+    public class Customer
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Mobile { get; set; }
+        public Location Location { get; set; }
+    }
+}
